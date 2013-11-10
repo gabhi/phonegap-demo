0ad76a4de09952487a7d5cc9ae7fe24ecc98edde
Abhijit Gaikwad <agaikwad@marketlive.com>
2013-11-10 00:41:04 -0800

    working version

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,11 @@
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope', '$location','$anchorScroll', function MyCtrl1($scope, $location, $anchorScroll) {
  	$scope.scrollTo = function(id) {
-      $location.hash(id);
-      $anchorScroll();
+
+ 	var activeEl = angular.element( document.querySelector( '#segmented-controller-data-items' ) );
+	activeEl.children().removeClass("active");      	
+      var myEl = angular.element( document.querySelector( '#'+id ) );
+      myEl.addClass("active")
    }
   }])
   .controller('MyCtrl2', [function() {
